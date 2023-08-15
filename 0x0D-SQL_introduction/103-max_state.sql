8969a918ac07b3034bb8e75867f236eff417e0b8
hajar <hajar.elrh@gmail.com>
2023-08-15 13:36:54 -0700

    UPDATED

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database a table dump
 -- Using the same data from task 18
--- Script that displays the max temperature of each state
+
 SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
-LIMIT 3;
+ORDER BY state;
