4ab2e4c9c10271db18c897352640768682fafc1b
hajar <hajar.elrh@gmail.com>
2023-08-16 08:52:38 -0700

    UPDATED

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Script that lists all privileges for 'user_0d_1' and 'user_0d_2'
+-- Scriot that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
