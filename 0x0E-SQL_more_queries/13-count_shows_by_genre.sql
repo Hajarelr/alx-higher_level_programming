bd7eb38ab0267378789f05609095507354b14949
hajar <hajar.elrh@gmail.com>
2023-08-16 10:11:02 -0700

    UPDATED

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- and display number of shows linked to each
 -- Each record should display tv_genres.name, number of shows linked to this genre
 -- Don't display a genre that doesn't have any shows list
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
--- Script that lists all shows and genres linked to the show from 'hbtn_0d_tvshows'
+-- Script that lists all shows and genres linked to the show
+-- from 'hbtn_0d_tvshows'
 -- If a show doesn't have a genre, display NULL in genre column
 -- Each record should display tv_shows.title, tv_genres.name
--- Results must be sorted in ascending order by the show title and genre name
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
