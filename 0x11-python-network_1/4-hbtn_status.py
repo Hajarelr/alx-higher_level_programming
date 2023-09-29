f642672352d07d7e1e1f00a1105f347779a369e5
Hajarelr <hajar.elrh@gmail.com>
2023-09-29 07:03:40 -0700

    PANDA

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-Script that fetches a given URL
-"""
-
-import requests
+"""Script that fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
